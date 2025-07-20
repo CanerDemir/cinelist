f2ec0a27f94ad10a21137b6e728eba12392e829f
Caner Demir <info.canerdemir@gmail.com>
2025-07-20 17:11:27 +0000

    The loader in the search bar of add new modal is not good enough. it is

--- src/components/cine/AddMediaDialog.tsx
+++ src/components/cine/AddMediaDialog.tsx
@@ -113,17 +113,20 @@ export function AddMediaDialog({
             onChange={handleInputChange}
             className="pl-10 pr-10"
           />
-          {isLoading && <Loader2 className="absolute right-3 top-1/2 -translate-y-1/2 h-5 w-5 animate-spin" />}
-          {!isLoading && searchTerm && (
+           <div className="absolute right-1 top-1/2 -translate-y-1/2 h-8 w-8 flex items-center justify-center">
+            {isLoading ? (
+              <Loader2 className="h-5 w-5 animate-spin text-muted-foreground" />
+            ) : searchTerm && (
               <Button
                 variant="ghost"
                 size="icon"
-                className="absolute right-1 top-1/2 -translate-y-1/2 h-8 w-8 rounded-full"
+                className="h-8 w-8 rounded-full"
                 onClick={handleClearSearch}
               >
                 <X className="h-5 w-5 text-muted-foreground" />
               </Button>
-          )}
+            )}
+          </div>
         </div>
         <ScrollArea className="h-[400px] mt-4">
           <div className="flex flex-col gap-4 pr-4">
